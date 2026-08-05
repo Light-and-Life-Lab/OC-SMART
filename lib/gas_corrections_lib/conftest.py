a1dca67190c0bee397bcb0fecf932a7d8263db2c
TimKindervatter <kindert1@tcnj.edu>
2026-08-05 15:44:44 -0400

    The gas corrections lib was for some reason added, remove it because it's supposed to be a submodule with different git history

--- lib/gas_corrections_lib/conftest.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-directory = os.path.abspath(os.path.dirname(__file__))
-sys.path.insert(0, directory + os.sep + "gas_corrections_cpp" + os.sep + "src")
-
-# This will run the build.py script in the src/ directory
-# The build script will compile the gas transmittance library and use cppimport to make the python module available for import
-# Doing this in conftest.py will ensure that the C++ library is built and up-to-date prior to running the tests
-import build
