a94c4fc0a5e29fad8ea2da8a13128fd1040b627f
TimKindervatter <timkindervatter@gmail.com>
2026-08-27 23:13:22 -0400

    Added a file for shared constants and added the dtype of the numpy arrays, which is now used in both OCSMART.py and mlnn.py. Changing this value will give a global switch of the dtype of all arrays across both files so that hunting down multiple places is not required if we want to change the dtype in the future.

---
+++ src/shared_constants.py
@@ -0,0 +1,8 @@
+"""
+Shared configuration constants for OC-SMART.
+
+Currently only array datatype, add any other constants shared across files here
+"""
+
+# Datatype for the pre-allocated arrays used in OCSMART.py and mlnn.py, set to float32 to save memory
+datatype = 'float32'
